b1947b7a0aeb5e1ef18d5271b0e12e666da3b371
Your Name <milenaclararizzi@gmail.com>
2024-07-09 18:01:18 -0300

    Delete styles.ts

--- src/components/Header/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const HeaderContainer = styled.header`
-  padding: 1.5rem;
-  text-align: center;
-  font-size: 1.8rem;
-  font-family: "Ubuntu", sans-serif;
-`;
