ced0543f942b5faf83255b45224fedfb8f289fe3
Your Name <milenaclararizzi@gmail.com>
2024-07-09 18:00:59 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { Home } from "./pages/Home";
 import { ContentWrapper, GlobalStyle } from "./global";
 import { RecipesProvider } from "./contexts/RecipeContext";
 import { FilterRecipeProvider } from "./contexts/FilterRecipeContext";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 function App() {
   return (
     <RecipesProvider>
       <FilterRecipeProvider>
-        <ContentWrapper>
-          <Home />
-          <GlobalStyle />
-        </ContentWrapper>
+        <BrowserRouter>
+          <ContentWrapper>
+            <Router />
+            <GlobalStyle />
+          </ContentWrapper>
+        </BrowserRouter>
       </FilterRecipeProvider>
     </RecipesProvider>
   );
